159996daa346b90c578dddf25200295e9e3ba96e
wormatefriendturkiye <wormatefriendsturkiye@gmail.com>
2025-03-24 00:55:06 +0300

    Update game3.js

--- js/game3.js
+++ js/game3.js
@@ -6090,7 +6090,7 @@ function applyGraphicsSettings(level, button) {
                     <li class="ui-tabs-tab ui-tab ui-tab-inactive0 ui-tab-active" style="margin: -5px">
                         <a href="javascript:void(0);" onclick="changeServer('peru')"> 
                             <span class="flag br" value="https://i.imgur.com/KE9354e.png">
-<div class="title-wormate-SEKO-flag" style=position: absolute;top: 0;z-index: 7;width: 127%;margin-left: 0px;margin-top: 1px;">
+<div class="title-wormate-SEKO-flag" style="position: absolute;top: 0;z-index: 1;width: 133%;margin-left: -10px;">
     <img src="https://haylamday.com/images/hiep_img/logo.png" width="20" align="center" alt="">Wormate Friends Türkiye.io
 </div>
                             </span> 
